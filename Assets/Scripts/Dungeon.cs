732b54a1c0e94dbd1e5a45c234c32de35b728175
Lasse Harm <lharm@lynet.de>
2019-02-25 17:25:50 +0100

    Added dungeon class and test.json

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Dungeon {
+
+    private string dungeonName = "DefaultDungeon";
+    private int floorCount = 5;
+    private List<int> enemyList = { 0, 1 };
+    private List<int> itemList = { 0 };
+    private int dungeonTheme = 0;
+    private List<int> specialFloors = { 99 };
+
+    public Dungeon
+        (
+            string dungeonNamen,
+            int floorCount,
+            List<int> enemyList,
+            List<int> itemList,
+            int dungeonTheme, 
+            List<int> specialFloors
+        )
+
+    {
+        this.dungeonName = dungeonName;
+        this.floorCount = floorCount;
+        this.enemyList = enemyList;
+        this.itemList = itemList;
+        this.dungeonTheme = dungeonTheme;
+        this.specialFloors = specialFloors;
+    }
+}
